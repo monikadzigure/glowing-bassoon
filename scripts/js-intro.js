bb0011ae307f7b538f5ee3b4b4d3f8b3f5b94620
Valdis Saules <valdis.s.coding@gmail.com>
2020-01-31 10:42:48 +0200

    js-introduduction example

---
+++ scripts/js-intro.js
@@ -0,0 +1,10 @@
+console.log("Hello my first JS script!");
+
+const para = document.querySelector("p");
+
+para.addEventListener("click", updateName);
+
+function updateName() {
+    let name = prompt("Enter a new name");
+    para.textContent = "Player 1: " + name;
+}
