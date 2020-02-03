b0a22162815a3127b1b0a8c9a0b150ab1462f809
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-03 11:21:13 +0200

    js-intro updated

--- scripts/js-intro.js
+++ scripts/js-intro.js
@@ -1,6 +1,20 @@
 console.log("Hello my first JS script!");
 let myStr = "Just some string";
 
+let myArr = [1, 2, 3, 5, 6];
+let myNum = myArr[3];
+let myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14]
+];
+console.log(myArray[3][2]);
+myArray.push();
+
+console.log(myArr.slice(3));
+console.log(myArr);
+console.log(myNum);
 // new style String interpolation with backticks
 console.log(`My string is: ${myStr} and its length is ${myStr.length}`);
 // this is just a commment
