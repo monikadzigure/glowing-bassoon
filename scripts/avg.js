bab5cc8b4c1d328ca0461e9dd9f45708a3839c84
Mona Dz <monikadzigure@inbox.lv>
2020-02-05 12:59:01 +0200

    Update avg.js

--- scripts/avg.js
+++ scripts/avg.js
@@ -4,7 +4,21 @@ function randomRange(myMin, myMax) {
 
 function getAverage(numThrows) {
     //write function to get an average value from dice throws
+   let total=0;
+   //write loop here
+   //use randomRange(1,6);
+   //loop goes here
+var myTrows;
+for(let i=0; i< numThrows; i++){
+    total +=randomRange(1,6);
+}
+   
+   randomRange(1,6);
+   total
+   return total/numThrows;
+
     return 3.5;
+    
 }
 
 let result = getAverage(1000);
